d79f2d724035de73804f66a615c1f83feb9a6576
Reiwil Lugo <reiwillugo@gmail.com>
2025-06-16 22:58:09 -0400

    Configure eas, Splash Screen & App's icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,36 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 // Apollo
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from '@graphql/client';
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "@graphql/client";
 // Hooks
-import { useColorScheme } from '@hooks/useColorScheme';
+import { useColorScheme } from "@hooks/useColorScheme";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  useEffect(() => {
+    async function hideSplash() {
+      if (loaded) {
+        await SplashScreen.hideAsync();
+      }
+    }
+    hideSplash();
+  }, [loaded]);
 
   if (!loaded) {
     return null;
@@ -21,7 +38,7 @@ export default function RootLayout() {
 
   return (
     <ApolloProvider client={apolloClient}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
